b561070c955ad82e7a548edf6324c95b6e97d4fa
Greg Turner <31353577+Greg-Turner@users.noreply.github.com>
2018-02-16 14:35:34 -0600

    Created models for Order and OrderProduct

---
+++ bangazonWebApp/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        public int PaymentId { get; set; }
+        public Payment Payment { get; set; }
+        
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateClosed { get; set; }
+
+        public virtual ICollection<OrderProduct> OrderProduct { get; set; }
+    }
+}
