d630fc668bb3fdab3962e10b6ad4566138c28104
Jason Figueroa <jason.figueroa2@gmail.com>
2018-02-16 14:17:51 -0600

    added models for ProductsCustomers & Recommendations

---
+++ bangazonWebApp/Models/ProductsCustomers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazonWebApp.Models
+{
+    public class ProductsCustomer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public bool LikeStatus { get; set; }
+
+        public int Rating { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        [Required]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
