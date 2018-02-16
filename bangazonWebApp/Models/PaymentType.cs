60f1c0812b7a961047355bea278a7c6b333fca5d
Kristen Norris <krnorris65@gmail.com>
2018-02-16 12:37:37 -0800

    created a PaymentType model. elminated Payments model since it was decided that it was unnecessary to have two tables for payment information

---
+++ bangazonWebApp/Models/PaymentType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazonWebApp.Models
+{
+    public class PaymentType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(12)]
+        public string Name { get; set; }
+
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+    }
+}
